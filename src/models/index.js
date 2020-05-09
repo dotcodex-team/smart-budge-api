3f82e187f969004d7945c98a86d3852b66181c41
Juan Rivera Vargas <jrivera.v@outlook.com>
2020-05-09 00:14:56 -0400

    without redis for moment

--- src/models/index.js
+++ src/models/index.js
@@ -1,17 +1,17 @@
 import fs from 'fs';
 import path from 'path';
 import Sequelize from 'sequelize';
-import Redis from 'ioredis';
-import RedisAdaptor from 'sequelize-transparent-cache-ioredis';
-import sequelizeCache from 'sequelize-transparent-cache';
+// import Redis from 'ioredis';
+// import RedisAdaptor from 'sequelize-transparent-cache-ioredis';
+// import sequelizeCache from 'sequelize-transparent-cache';
 
-const redis = new Redis();
-const redisAdaptor = new RedisAdaptor({
-  client: redis,
-  namespace: 'model',
-  lifetime: 60 * 60
-});
-const { withCache } = sequelizeCache(redisAdaptor);
+// const redis = new Redis();
+// const redisAdaptor = new RedisAdaptor({
+//   client: redis,
+//   namespace: 'model',
+//   lifetime: 60 * 60
+// });
+// const { withCache } = sequelizeCache(redisAdaptor);
 
 const env = process.env.NODE_ENV || 'development';
 // eslint-disable-next-line
@@ -39,7 +39,8 @@ const models = Object.assign(
       const model = require(path.join(__dirname, file)).default;
 
       return {
-        [model.name]: withCache(model.init(sequelize, Sequelize))
+        // [model.name]: withCache(model.init(sequelize, Sequelize))
+        [model.name]: model.init(sequelize, Sequelize)
       };
     })
 );
